312d496bf19169bbdf1ee93bfd55c5631d0c99bf
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-04 19:07:08 -0300

    Creates middlewares to handler routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const notFound = require('./middlewares/notFound');
+const errorHandler = require('./middlewares/errorHandler');
 
 app.use('/', routes);
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
