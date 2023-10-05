63c625dd9444da2adc3c46b3772877b7b5c2eee4
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-05 10:04:53 -0300

    Creates tests to integrationService.js

---
+++ api/integration/pokeapiService.test.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const { getPokemonData } = require('./pokeapiService');
+const PokemonNotFoundException = require('../exceptions/PokemonNotFoundException');
+const PokemonDataFetchException = require("../exceptions/PokemonDataFetchException");
+const dittoMock = require('../mocks/dittoIntegrationMock'); // Import the mock data
+
+describe('pokeapiService', () => {
+    const mock = new MockAdapter(axios);
+
+    afterEach(() => {
+        mock.reset();
+    });
+
+    it('should fetch pokemon data successfully', async () => {
+        mock.onGet('https://pokeapi.co/api/v2/pokemon/ditto').reply(200, dittoMock);
+
+        const result = await getPokemonData('ditto');
+        expect(result).toEqual(dittoMock);
+    });
+
+    it('should throw PokemonNotFoundException for 404 response', async () => {
+        mock.onGet('https://pokeapi.co/api/v2/pokemon/unknown').reply(404);
+
+        await expect(getPokemonData('unknown')).rejects.toThrow(PokemonNotFoundException);
+    });
+
+    it('should throw PokemonDataFetchException for non-404 errors', async () => {
+        mock.onGet('https://pokeapi.co/api/v2/pokemon/errorpokemon').reply(500);
+
+        await expect(getPokemonData('errorpokemon')).rejects.toThrow(PokemonDataFetchException);
+    });
+});
---
+++ api/mocks/dittoIntegrationMock.js
@@ -0,0 +1,463 @@
+const dittoIntegrationMock = {
+    "abilities": [
+        {
+            "ability": {
+                "name": "limber",
+                "url": "https://pokeapi.co/api/v2/ability/7/"
+            },
+            "is_hidden": false,
+            "slot": 1
+        },
+        {
+            "ability": {
+                "name": "imposter",
+                "url": "https://pokeapi.co/api/v2/ability/150/"
+            },
+            "is_hidden": true,
+            "slot": 3
+        }
+    ],
+    "base_experience": 101,
+    "forms": [
+        {
+            "name": "ditto",
+            "url": "https://pokeapi.co/api/v2/pokemon-form/132/"
+        }
+    ],
+    "game_indices": [
+        {
+            "game_index": 76,
+            "version": {
+                "name": "red",
+                "url": "https://pokeapi.co/api/v2/version/1/"
+            }
+        },
+        {
+            "game_index": 76,
+            "version": {
+                "name": "blue",
+                "url": "https://pokeapi.co/api/v2/version/2/"
+            }
+        },
+        {
+            "game_index": 76,
+            "version": {
+                "name": "yellow",
+                "url": "https://pokeapi.co/api/v2/version/3/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "gold",
+                "url": "https://pokeapi.co/api/v2/version/4/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "silver",
+                "url": "https://pokeapi.co/api/v2/version/5/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "crystal",
+                "url": "https://pokeapi.co/api/v2/version/6/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "ruby",
+                "url": "https://pokeapi.co/api/v2/version/7/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "sapphire",
+                "url": "https://pokeapi.co/api/v2/version/8/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "emerald",
+                "url": "https://pokeapi.co/api/v2/version/9/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "firered",
+                "url": "https://pokeapi.co/api/v2/version/10/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "leafgreen",
+                "url": "https://pokeapi.co/api/v2/version/11/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "diamond",
+                "url": "https://pokeapi.co/api/v2/version/12/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "pearl",
+                "url": "https://pokeapi.co/api/v2/version/13/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "platinum",
+                "url": "https://pokeapi.co/api/v2/version/14/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "heartgold",
+                "url": "https://pokeapi.co/api/v2/version/15/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "soulsilver",
+                "url": "https://pokeapi.co/api/v2/version/16/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "black",
+                "url": "https://pokeapi.co/api/v2/version/17/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "white",
+                "url": "https://pokeapi.co/api/v2/version/18/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "black-2",
+                "url": "https://pokeapi.co/api/v2/version/21/"
+            }
+        },
+        {
+            "game_index": 132,
+            "version": {
+                "name": "white-2",
+                "url": "https://pokeapi.co/api/v2/version/22/"
+            }
+        }
+    ],
+    "height": 3,
+    "held_items": [
+        {
+            "item": {
+                "name": "metal-powder",
+                "url": "https://pokeapi.co/api/v2/item/234/"
+            },
+            "version_details": [
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "ruby",
+                        "url": "https://pokeapi.co/api/v2/version/7/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "sapphire",
+                        "url": "https://pokeapi.co/api/v2/version/8/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "emerald",
+                        "url": "https://pokeapi.co/api/v2/version/9/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "firered",
+                        "url": "https://pokeapi.co/api/v2/version/10/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "leafgreen",
+                        "url": "https://pokeapi.co/api/v2/version/11/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "diamond",
+                        "url": "https://pokeapi.co/api/v2/version/12/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "pearl",
+                        "url": "https://pokeapi.co/api/v2/version/13/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "platinum",
+                        "url": "https://pokeapi.co/api/v2/version/14/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "heartgold",
+                        "url": "https://pokeapi.co/api/v2/version/15/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "soulsilver",
+                        "url": "https://pokeapi.co/api/v2/version/16/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "black",
+                        "url": "https://pokeapi.co/api/v2/version/17/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "white",
+                        "url": "https://pokeapi.co/api/v2/version/18/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "black-2",
+                        "url": "https://pokeapi.co/api/v2/version/21/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "white-2",
+                        "url": "https://pokeapi.co/api/v2/version/22/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "x",
+                        "url": "https://pokeapi.co/api/v2/version/23/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "y",
+                        "url": "https://pokeapi.co/api/v2/version/24/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "omega-ruby",
+                        "url": "https://pokeapi.co/api/v2/version/25/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "alpha-sapphire",
+                        "url": "https://pokeapi.co/api/v2/version/26/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "sun",
+                        "url": "https://pokeapi.co/api/v2/version/27/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "moon",
+                        "url": "https://pokeapi.co/api/v2/version/28/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "ultra-sun",
+                        "url": "https://pokeapi.co/api/v2/version/29/"
+                    }
+                },
+                {
+                    "rarity": 5,
+                    "version": {
+                        "name": "ultra-moon",
+                        "url": "https://pokeapi.co/api/v2/version/30/"
+                    }
+                }
+            ]
+        },
+        {
+            "item": {
+                "name": "quick-powder",
+                "url": "https://pokeapi.co/api/v2/item/251/"
+            },
+            "version_details": [
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "diamond",
+                        "url": "https://pokeapi.co/api/v2/version/12/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "pearl",
+                        "url": "https://pokeapi.co/api/v2/version/13/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "platinum",
+                        "url": "https://pokeapi.co/api/v2/version/14/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "heartgold",
+                        "url": "https://pokeapi.co/api/v2/version/15/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "soulsilver",
+                        "url": "https://pokeapi.co/api/v2/version/16/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "black",
+                        "url": "https://pokeapi.co/api/v2/version/17/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "white",
+                        "url": "https://pokeapi.co/api/v2/version/18/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "black-2",
+                        "url": "https://pokeapi.co/api/v2/version/21/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "white-2",
+                        "url": "https://pokeapi.co/api/v2/version/22/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "x",
+                        "url": "https://pokeapi.co/api/v2/version/23/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "y",
+                        "url": "https://pokeapi.co/api/v2/version/24/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "omega-ruby",
+                        "url": "https://pokeapi.co/api/v2/version/25/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "alpha-sapphire",
+                        "url": "https://pokeapi.co/api/v2/version/26/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "sun",
+                        "url": "https://pokeapi.co/api/v2/version/27/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "moon",
+                        "url": "https://pokeapi.co/api/v2/version/28/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "ultra-sun",
+                        "url": "https://pokeapi.co/api/v2/version/29/"
+                    }
+                },
+                {
+                    "rarity": 50,
+                    "version": {
+                        "name": "ultra-moon",
+                        "url": "https://pokeapi.co/api/v2/version/30/"
+                    }
+                }
+            ]
+        }
+    ]
+};
+
+module.exports = dittoIntegrationMock;
